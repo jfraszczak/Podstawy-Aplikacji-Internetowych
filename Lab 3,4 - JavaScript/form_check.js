958b8671f5a12c6667ca4fddceee2f64fd4a23d4
jfraszczak <48968126+jfraszczak@users.noreply.github.com>
2020-03-25 15:33:16 +0100

    Add files via upload

---
+++ Lab 3,4 - JavaScript/form_check.js
@@ -0,0 +1,89 @@
+function isWhiteSpaceOrEmpty(str) {
+    return /^[\s\t\r\n]*$/.test(str);
+}
+
+function isEmailInvalid(str) {
+    let email = /^[a-zA-Z_0-9\.]+@[a-zA-Z_0-9\.]+\.[a-zA-Z][a-zA-Z]+$/;
+    if (email.test(str))
+        return false;
+    return true;
+}
+
+function checkStringAndFocus(obj, msg, func) {
+    let str = obj.value;
+    let errorFieldName = "e_" + obj.name.substr(2, obj.name.length);
+    if (func(str)) {
+        document.getElementById(errorFieldName).innerHTML = msg;
+        obj.focus();
+        return false;
+    }
+    else {
+        document.getElementById(errorFieldName).innerHTML = "";
+        return true;
+    }
+}
+
+function validate(form){
+    var fields = ["f_imie", "f_nazwisko", "f_kod", "f_ulica", "f_miasto", "f_email"];
+    var messages = ["Podaj imię!", "Podaj nazwisko!", "Podaj kod pocztowy!", "Podaj ulicę!", "Podaj miasto!", "Podaj poprawny email!"];
+    var noErrors = true;
+    var func;
+    for(var i = 0; i < fields.length; i++){
+        if(fields[i] == "f_email") func = isEmailInvalid;
+        else func = isWhiteSpaceOrEmpty;
+        if(!checkStringAndFocus(form.elements[fields[i]], messages[i], func)) noErrors = false;
+    }
+
+    return noErrors
+}
+
+function showElement(e) {
+    document.getElementById(e).style.visibility = 'visible';
+}
+
+function hideElement(e) {
+    document.getElementById(e).style.visibility = 'hidden';
+}
+
+function alterRows(i, e) {
+    if (e) {
+        if (i % 2 == 1) {
+            e.setAttribute("style", "background-color: Aqua;");
+        }
+        e = e.nextSibling;
+        while (e && e.nodeType != 1) {
+            e = e.nextSibling;
+        }
+        alterRows(++i, e);
+    }
+}
+
+function nextNode(e) {
+    while (e && e.nodeType != 1) {
+        e = e.nextSibling;
+    }
+    return e;
+}
+
+function prevNode(e) {
+    while (e && e.nodeType != 1) {
+        e = e.previousSibling;
+    }
+    return e;
+}
+
+function swapRows(b) {
+    let tab = prevNode(b.previousSibling);
+    let tBody = nextNode(tab.firstChild);
+    let lastNode = prevNode(tBody.lastChild);
+    tBody.removeChild(lastNode);
+    let firstNode = nextNode(tBody.firstChild);
+    tBody.insertBefore(lastNode, firstNode);
+}
+
+function cnt(form, msg, maxSize) {
+    if (form.value.length > maxSize)
+        form.value = form.value.substring(0, maxSize);
+    else
+        msg.innerHTML = maxSize - form.value.length;
+}
