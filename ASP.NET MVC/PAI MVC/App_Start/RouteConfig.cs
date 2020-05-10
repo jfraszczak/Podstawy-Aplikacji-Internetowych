d68b55f3567227f1cda8d6825faf973b7fede108
jfraszczak <jakub.fraszczak@git.com>
2020-05-10 18:53:58 +0200

    :3

---
+++ ASP.NET MVC/PAI MVC/App_Start/RouteConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace PAI_MVC
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Square",
+                url: "Songs/Square/{id}",
+                defaults: new { controller = "Songs", action = "Square", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Songs", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Square-23",
+                url: "",
+                defaults: new { controller = "Songs", action = "Square", id = 23 }
+            );
+        }
+    }
+}
---
+++ ASP.NET MVC/PAI MVC/Models/Genre.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PAI_MVC.Models
+{
+    public class Genre
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required!")]
+        [StringLength(100, ErrorMessage = "Maximal length of the name of a genre is 100 characters!")]
+        public string Name { get; set; }
+        public ICollection<Song> SongsList { get; set; }
+    }
+}
